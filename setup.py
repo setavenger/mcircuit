8a2ecf0a8d67422ae8eae81adb702d446104b595
S.Blagogee <34041358+setavenger@users.noreply.github.com>
2023-10-12 10:28:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
-# Read the contents of the requirements file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='mcircuit',
     version='0.1',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        'autopep8==1.5.7',
+        'bidict==0.21.2',
+        'decorator==4.4.2',
+        'llvmlite==0.36.0',
+        'networkx==2.5.1',
+        'pycodestyle==2.7.0',
+        'PySide6==6.1.0',
+        'shiboken6==6.1.0',
+        'toml==0.10.2',
+    ],
 )
